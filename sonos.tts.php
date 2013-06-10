32dad7fc6a67772f6a8db9f23542aca4e1f656ba
DjMomo <fabien@fmaurel.fr>
2013-06-10 20:46:46 +0200

    TTS example

---
+++ sonos.tts.php
@@ -0,0 +1,16 @@
+<?php
+// Exemple de l'utilisation de la fonction Text-to-speech
+require("sonos.class.php");
+$IP_sonos_1 = "192.168.1.11"; // A adapter avec l'adresse IP du Sonos à contrôler
+ 
+$volume = 0;
+$force_unmute = 0;
+ 
+if (isset($_GET['force_unmute'])) $force_unmute = $_GET['force_unmute']; // Force la désactivation de la sourdine. Optionnel
+if (isset($_GET['volume'])) $volume = $_GET['volume']; // Niveau sonore. Optionnel.
+$message = $_GET['message']; // Message à diffuser
+ 
+//Instanciation de la classe
+$sonos_1 = new SonosPHPController($IP_sonos_1);
+$sonos_1->PlayTTS($message,$directory,$volume,$force_unmute); //Lecture du message
+?>
