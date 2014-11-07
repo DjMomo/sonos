576295798efb7e55281f56850b1c94cc226eeaea
Andrew Gillis <andrew@vortexbox.org>
2014-11-06 23:21:37 -0500

    Created add music library (Samba share) function AddMusicLibrary()

--- sonos.class.php
+++ sonos.class.php
@@ -333,6 +333,21 @@ class SonosPHPController
 		$args = '<AlbumArtistDisplayOption/>';
 		return $this->Upnp($url,$service,$action,$args);
 	}
+	
+	/**
+	* Add music library (Samba share)
+	*
+	*/
+	public function AddMusicLibrary($libraryLocation)
+	{
+		$url = '/MediaServer/ContentDirectory/Control';
+		$action = 'CreateObject';
+		$service = 'urn:schemas-upnp-org:service:ContentDirectory:1';
+		$meta = '<DIDL-Lite xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:upnp="urn:schemas-upnp-org:metadata-1-0/upnp/" xmlns:r="urn:schemas-rinconnetworks-com:metadata-1-0/" xmlns="urn:schemas-upnp-org:metadata-1-0/DIDL-Lite/"><container id="" restricted="false"><dc:title>'.$libraryLocation.'</dc:title><r:usernameX/><r:passwordX/></container></DIDL-Lite>';
+		$meta = htmlentities($meta);
+		$args = '<ContainerID>S:</ContainerID><Elements>'.$meta.'</Elements>';
+		return $this->Upnp($url,$service,$action,$args);
+	}
 
 	/**
 	* Split string in several strings
